ae0cdf038c30990649fa3261668f4ae37027ed6a
crmell <jessicaseren26@gmail.com>
2024-10-23 21:30:33 +0700

    add logout, login, regis, validation

--- video.php
+++ video.php
@@ -1,3 +1,7 @@
+<?php
+  session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -5,36 +9,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Video</title>
 
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-    />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
     <link rel="stylesheet" href="css/style.css" />
   </head>
+
   <body>
   <header class="header">
       <section class="flex">
         <a href="/home.php" class="logo">CyberCourse</a>
         <form action="" method="post" class="search-form">
-          <input
-            type="text"
-            name="search_box"
-            placeholder="search courses..."
-            required
-            maxlength="100"
-          />
-          <button
-            type="submit"
-            class="fas fa-search"
-            name="search_box"
-          ></button>
+          <input type="text" name="search_box" placeholder="search courses..." required maxlength="100"/>
+          <button type="submit" class="fas fa-search" name="search_box"></button>
         </form>
+
         <div class="icons">
           <div id="menu-btn" class="fas fa-bars"></div>
           <div id="search-btn" class="fas fa-search"></div>
           <div id="user-btn" class="fas fa-user"></div>
           <div id="toggle-btn" class="fas fa-sun"></div>
         </div>
+
         <div class="profile">
           <img src="img/profile.jpeg" alt="" />
           <h3>Panda</h3>
