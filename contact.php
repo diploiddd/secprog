b151949c71e4e21a577baa3a9f7070e5151564f0
crmell <jessicaseren26@gmail.com>
2024-10-24 08:42:41 +0700

    nav bar

--- contact.php
+++ contact.php
@@ -67,18 +67,10 @@
 
       <nav class="navbar">
         <a href="home.php"><i class="fas fa-home"></i><span>Home</span></a>
-        <a href="about.php"
-          ><i class="fas fa-question"></i><span>About Us</span></a
-        >
-        <a href="course.php"
-          ><i class="fas fa-graduation-cap"></i><span>Courses</span></a
-        >
-        <a href="teachers.php"
-          ><i class="fas fa-chalkboard-user"></i><span>Teachers</span></a
-        >
-        <a href="contact.php"
-          ><i class="fas fa-headset"></i><span>Contact Us</span></a
-        >
+        <a href="about.php"><i class="fas fa-question"></i><span>About Us</span></a>
+        <a href="course.php"><i class="fas fa-graduation-cap"></i><span>Courses</span></a>
+        <a href="teachers.php"><i class="fas fa-chalkboard-user"></i><span>Teachers</span></a>
+        <a href="contact.php"><i class="fas fa-headset"></i><span>Contact Us</span></a>
       </nav>
     </div>
 
