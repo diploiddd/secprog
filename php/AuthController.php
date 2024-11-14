567d872901b77bc5a5c1f086ac4c87fd6f76628f
lemonade4415 <nadelineaprillini@gmail.com>
2024-11-14 14:37:57 +0700

    'email

--- php/AuthController.php
+++ php/AuthController.php
@@ -4,41 +4,45 @@
 
     if($_SERVER['REQUEST_METHOD'] == "POST"){
         if(isset($_POST['login'])){
-            //mysqli_real_escape_string -> salah satu cara ngecegah sqlinjection
-            $email = mysqli_real_escape_string($conn, $_POST['email']);
-            $password = mysqli_real_escape_string($conn, $_POST['password']);
             
-            $query = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
-            $result = $conn->query($query) or die("An error occured. Please try again later :(");
-
-            $row = mysqli_fetch_assoc($result);
-            if(is_array($row) && !empty($row)){
-                $_SESSION['valid'] = $row['email'];
-                $_SESSION['username'] = $row['username'];
-                $_SESSION['password'] = $row['password'];
-            }
-            else{
-                echo "<div class='message'>
-                    <p>Wrong credential!<p>
-                    <div> <br>";
-                echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
-            }
-
-
-            if(isset($_SESSION['valid'])){
-                header("Location: ../home.php");
-            }
-            else{
-
-            }
-        }
-        else if (isset($_POST['register'])){
-            $username = $_POST['username'];
-            $email = $_POST['email'];
+            $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
             $password = $_POST['password'];
 
-            $query = "SELECT email FROM users WHERE email = '$email'";
-            $result = $conn->query($query) or die("An error has occured. Please try again later :(");
+            //Prepared Statement
+            $query = "SELECT * FROM users WHERE email = ?";
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $result = $stmt->get_result() or die("An error occured. Please try again later :(");
+
+            if($result->num_rows == 1){
+                $row = $result->fetch_assoc();
+                if(md5($password) === $row['password']){
+                    $_SESSION['valid'] = $row['email'];
+                    $_SESSION['username'] = $row['username'];
+                    $_SESSION['password'] = $row['password'];
+                    header("Location: ../home.php");
+                } else echo "Incorrect Email or Password";
+            } else echo "Incorrect Email or Passwordnn";
+            
+            $stmt->close();
+        }
+
+        else if (isset($_POST['register'])){
+            $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+            $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+            $password = md5($_POST['password']);
+
+            if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
+                echo "Invalid username!";
+                exit();
+            }
+
+            $query = "SELECT email FROM users WHERE email = ?";
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $result = $stmt->get_result() or die("An error occured. Please try again later :(");
 
             if($result->num_rows != 0){
                 echo "<div class='message'>
@@ -47,13 +51,18 @@
                 echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
             }
             else{
-                mysqli_query($conn, "INSERT INTO users(username, email, password) VALUES ('$username', '$email', '$password')") or die("An error has occured. Please try again later :(");
-                echo "<div class='message'>
-                    <p>Registration success!<p>
-                    <div> <br>";
-                echo "<a href='./login.php'><button class='btn'>Login</button>";
+                $query2 = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+                $stmt1 = $conn->prepare($query2);
+                $stmt1->bind_param("sss", $username, $email, $password);
+                if($stmt1->execute()){
+                    header("Location: ../login.php");
+                } else{
+                    echo "Registration error! Please try again later.";
+                    header("Location: ./home.php");
+                }
             }
         }
+        
         else if (isset($_POST['logout'])){
 
         }
